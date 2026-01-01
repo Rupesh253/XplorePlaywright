0c8d665f93003876caa1118f209dee0278373d4c
Rupesh Kumar Somala <rupesharyan250@gmail.com>
2026-01-01 22:53:23 +0530

    Create loginPage.ts

---
+++ test_pages/pom_approach1/loginPage.ts
@@ -0,0 +1,42 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  private page: Page;
+  public LoginPage(page: Page) {
+    this.page = page;
+  }
+  //page elements as properties and are initialized upon call.
+  private get header_label() {
+    return this.page.locator(`.login_logo`);
+  }
+  private get username_textbox() {
+    return this.page.locator(`#user-name`);
+  }
+  private get password_textbox() {
+    return this.page.locator(`#password`);
+  }
+  private get login_button() {
+    return this.page.locator(`#login-button`);
+  }
+  private get error_label(){
+    return this.page.locator('[data-test="error"]');
+  }
+  async inputUserName(userName: string) {
+    await this.username_textbox.fill(userName);
+  }
+  async inputPassword(password: string) {
+    await this.password_textbox.fill(password);
+  }
+  async clickLogin() {
+    await this.login_button.click();
+  }
+  async doLogin(userName: string, password: string) {
+    //Facaded
+    await this.inputUserName(userName);
+    await this.inputPassword(password);
+    await this.clickLogin();
+  }
+  async getValidationError(){
+    await this.error_label.innerText();
+  }
+}
